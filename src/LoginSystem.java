69d62cde5932ac0f41aa3b78fb0647e9f8539cee
Salah AH <salah322s1@gmail.com>
2022-03-07 13:35:46 +0800

    .

--- src/LoginSystem.java
+++ src/LoginSystem.java
@@ -3,22 +3,20 @@ import java.io.*;
 import java.nio.file.*;
 
 
+
 public class LoginSystem {
     private static Scanner sc;
     public static void main(String[] args)throws IOException{
+        ArrayList<Credentials> credentials = readCredsFromFile();      
+        createNewUsers();
         
-          ArrayList<Credentials> credentials=readCredsFromFile();
-          saveCredsToFile(credentials);
-          for (int i = 0; i < credentials.size();i++){
-            System.out.println(credentials.get(i));
-           
-          }     
+
    }
 
    private static ArrayList<Credentials> readCredsFromFile() throws IOException {
        ArrayList<Credentials> credentials = new ArrayList<>();
 
-       List<String> lines = Files.readAllLines(Paths.get("credentials.csv"));
+       List<String> lines = Files.readAllLines(Paths.get("src/credentials.csv"));
        for (int i = 0; i< lines.size();i++) {
            String[] items = lines.get(i).split(",");
            int password = Integer.parseInt(items[0]);
@@ -27,14 +25,36 @@ public class LoginSystem {
        }
        return credentials;
    }
+
+   //This method saves input into credentials.csv and should be used for signing up method
    private static void saveCredsToFile(ArrayList<Credentials> credentials) throws IOException {
        StringBuilder sb = new StringBuilder();
        for (int i = 0; i < credentials.size(); i++){
            sb.append(credentials.get(i).toCSVString()+"\n");
        }
-       Files.write(Paths.get("credentials.csv"), sb.toString().getBytes());
+       Files.write(Paths.get("src/credentials.csv"), sb.toString().getBytes());
    }
-  
+
+   //This method creates a new username and sign them up in the system and will be saved to credentials .csv
+   private static void createNewUsers() throws IOException {
+       int  temppassword;
+       ArrayList<Credentials> credentials=readCredsFromFile();
+       sc = new Scanner(System.in);
+       System.out.println("Please Enter your new Username: ");
+       String username = sc.nextLine();
+       System.out.println("Please Enter your new password");
+       int password = sc.nextInt();
+       System.out.println("Please Enter your password again: ");
+       temppassword=sc.nextInt();
+        do {
+            System.out.println("Passwords don't match. Please Enter your new password again");
+            temppassword=sc.nextInt();
+        } while(temppassword!=password);
+
+        System.out.println("Entry successful!, Your info has been saved in our system");
+        
+       credentials.add(new Credentials(password,username));
+   }
 
 
 }
