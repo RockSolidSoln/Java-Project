3775fe31b8bed608ff4890dd82239c4279301b61
Salah AH <salah322s1@gmail.com>
2022-03-13 03:05:32 +0800

    Donor almost done 1 function left

--- src/donorMenu.java
+++ src/donorMenu.java
@@ -2,7 +2,9 @@ import java.util.*;
 import java.io.IOException;
 
 public class donorMenu {
+    
    public static void showDonormenu(String name){
+    Scanner sc = new Scanner(System.in);
     String menu=
     ("\n|------------------------------------------------------------|"
     +"\n|                 Welcome Donor " +name+ "                   |"
@@ -12,13 +14,23 @@ public class donorMenu {
     +"\n|------------------------------------------------------------|");
     
     System.out.println(menu);
-    int choice; 
+    int choice= sc.nextInt();
     switch (choice) {
-        case value:
+        case(1):    System.out.println("Please Enter your name: ");
+                    String Name = sc.nextLine();
+                    System.out.println("Please Enter phone number: ");
+                    String phoneNumber = sc.nextLine();
+                    System.out.println("Please Enter the item name: ");
+                    String itemName = sc.nextLine();
+                    System.out.println("Please Enter How many items you want to donate");
+                    int quantity = sc.nextInt();
+                    
+                    Donor set = new Donor(name,phoneNumber,itemName,quantity);
+                    set.saveDonorInfo();
             
-            break;
+            
     
-        case
+        case(2):
             break;
    }
    }
