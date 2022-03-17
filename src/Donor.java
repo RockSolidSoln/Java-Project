c02f1c493f7d662712f8ebb534d0fc73232a64a3
Salah AH <salah322s1@gmail.com>
2022-03-18 05:11:11 +0800

    all done in ngo and donor just improve and route

--- src/Donor.java
+++ src/Donor.java
@@ -6,13 +6,15 @@ public class Donor{
     private String phoneNumber;
     private String itemName;
     private String quantity;
+    private String ngo;
     public Donor(){};
 
-    public Donor(String name,String phoneNumber,String itemName, String qty){
+    public Donor(String name,String phoneNumber,String itemName, String qty,String ngo){
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.itemName = itemName;
-        this.quantity = qty;       
+        this.quantity = qty;   
+        this.ngo = ngo;    
     }
     // each of the statements below will return a variable we will use on later.
     public String getName(){                    
@@ -27,18 +29,20 @@ public class Donor{
     public String getQuantity(){
         return quantity;
     }
+    public String getNgo(){
+        return ngo;
+    }
 
     // this method will save all the information about a donor and will be used in a html report later on!
     public void saveDonorInfo(){
         try{
-            String path  = "src/Documentation/Donorinfo.csv";
+            String path  = "src/Documentation/DonatedItems.csv";
             FileWriter fw = new FileWriter(path,true);
-            fw.write("\n"+ name +","+ phoneNumber+"," +itemName+"," + quantity);
+            fw.write("\n"+ name + "," + phoneNumber + "," +itemName+ "," + quantity + "," +ngo);
             fw.close();
         }catch(IOException e){
             System.err.println("IOException" + e.getMessage());
-        }
-        
+        }      
 
     }
 }
--- src/donorMenu.java
+++ src/donorMenu.java
@@ -10,15 +10,12 @@ import java.awt.event.*;
 
 
 public class donorMenu {
-    public static void main(String[] args) throws IOException{
-        viewAidsDonated();
-    }
-    
+//-------------------------------------Shows Donor Menu and re-routes to enter aids or view all aids donated.-----------------------------------------------------------------------//
    public static void showDonormenu(String name) throws IOException{
     Scanner sc = new Scanner(System.in);
     String menu=
     ("\n|------------------------------------------------------------|"
-    +"\n|                 Welcome Donor " +name+ "                   |"
+    +"\n|                 Welcome Donor "+name+"                        |"
     +"\n|------------------------------------------------------------|"
     +"\n|      Enter 1: To Enter the aids to be donated              |"
     +"\n|      Enter 2: To View list of all aids donated:            |"
@@ -28,16 +25,17 @@ public class donorMenu {
     int choice= sc.nextInt();
     switch (choice) {
         case(1):     
-            enterAids(name); //re-routes to the enter aids method 
-            
+            enterAids(name); //re-routes to the enter aids method.
+            break;
             
     
         case(2):
-            viewAidsDonated();
+            viewAidsDonated(); //re-routes to view all aids donated.
             break;
    }
    }
-
+   //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+   //---------------------------------------------Allows User to enter aids to donate along with donor info-------------------------------------------------------------------------//
    public static void enterAids(String name){
     Scanner sc = new Scanner(System.in);
     System.out.println("Please Enter phone number: ");
@@ -46,17 +44,21 @@ public class donorMenu {
     String itemName = sc.nextLine();
     System.out.println("Please Enter How many items you want to donate");
     String quantity = sc.nextLine();
+    System.out.println("Please Enter The None Goverment Orginization you would like to donate to:");
+    String ngo = sc.nextLine();
     
-    Donor set = new Donor(name,phoneNumber,itemName,quantity);
+    
+    Donor set = new Donor(name,phoneNumber,itemName,quantity,ngo);
     set.saveDonorInfo();
    }
-    private static void viewAidsDonated() throws IOException {
-
-     
-        File file = new File("src/Documentation/Donorinfo.csv");
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+    //--------------------------------------------Allows the user to view all aids donated-------------------------------------------------------------------------------------------//
+    private static void  viewAidsDonated() throws IOException {
+ 
+        File file = new File("src/Documentation/DonatedItems.csv");
         String line = "";
         
-        System.out.format("%8s%20s%9s%16s","Name","PhoneNumber","aid","quantity"+"\n");
+        System.out.format("%8s%20s%9s%16s%13s","Name","PhoneNumber","Aid","Quantity","Ngo"+"\n");
         try{
            Scanner input = new Scanner(file);
            BufferedReader br = new BufferedReader(new FileReader(file));
@@ -73,17 +75,14 @@ public class donorMenu {
            } catch (Exception e) {
                 e.printStackTrace();
            
+        }        
         }
-            
-        }
-    
-
-        
-         
-
-    } 
-    
-   
-   
-
+                     
+     
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+   //-----------------------------------------------This Function Saves HTML Report--------------------------------------------------------------------------------------------------//
+   public static void saveHTMLReport(){
+       File file = new File("src/Documentation/report.html");
+   }
+}
 
