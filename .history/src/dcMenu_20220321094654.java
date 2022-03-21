09979b2e745df4ad41cc1a8d6ad364d45c5474b0
Ayaan Ahmad <76241848+ayaan278@users.noreply.github.com>
2022-03-21 09:55:31 +0400

    dc menu functional

---
+++ .history/src/dcMenu_20220321094654.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.IOException;
+import java.io.*;
+
+public class dcMenu {
+    public static void showDcMenu() throws IOException {
+        System.out.println
+        ("\n|---------------------------------------------------------------|"
+        +"\n|               Welcome to Distribution Center                  |"
+        +"\n|---------------------------------------------------------------|"
+        +"\n|     Enter 1: To view all the aids available in DC             |"
+        +"\n|     Enter 2: To view all the NGO's demand                     |"
+        +"\n|     Enter 3: To match the aids available to NGO               |"
+        +"\n|     Enter 4: To view all the aids donated by specific Donor   |"
+        +"\n|     Enter 5: To view all the aids recieve by specific NGO     |"
+        +"\n|     Enter 6: To show the history of all the aids distributed  |"
+        +"\n|---------------------------------------------------------------|");
+        
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1: donorMenu.showStatus("null",1);
+                    break;
+            case 2: ngoMenu.showStatus("null",1);
+                    break;
+            case 3: 
+                    break;
+            case 4: 
+                    break;
+            case 5: 
+                    break;
+            case 6:     
+                    break;
+        }
+        sc.close();
+    }
+    public static void show(int flag){
+        Scanner sc = new Scanner(System.in);
+        if(flag==1){
+                System.out.println("Please Enter the name-");
+                String str = sc.nextLine();
+                
+        }
+        else if(flag==2){
+
+        }
+        sc.close();
+    }
+}
---
+++ .history/src/dcMenu_20220321094728.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.IOException;
+import java.io.*;
+
+public class dcMenu {
+    public static void showDcMenu() throws IOException {
+        System.out.println
+        ("\n|---------------------------------------------------------------|"
+        +"\n|               Welcome to Distribution Center                  |"
+        +"\n|---------------------------------------------------------------|"
+        +"\n|     Enter 1: To view all the aids available in DC             |"
+        +"\n|     Enter 2: To view all the NGO's demand                     |"
+        +"\n|     Enter 3: To match the aids available to NGO               |"
+        +"\n|     Enter 4: To view all the aids donated by specific Donor   |"
+        +"\n|     Enter 5: To view all the aids recieve by specific NGO     |"
+        +"\n|     Enter 6: To show the history of all the aids distributed  |"
+        +"\n|---------------------------------------------------------------|");
+        
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1: donorMenu.showStatus("null",1);
+                    break;
+            case 2: ngoMenu.showStatus("null",1);
+                    break;
+            case 3: 
+                    break;
+            case 4: 
+                    break;
+            case 5: 
+                    break;
+            case 6:     
+                    break;
+        }
+        sc.close();
+    }
+    public static void show(int flag){
+        Scanner sc = new Scanner(System.in);
+        String str;
+        if(flag==1){
+                System.out.println("Please Enter the name -");     
+        }
+        else if(flag==2){
+
+        }
+        str = sc.nextLine();
+        showHistory.viewAidsHistory(str);
+        sc.close();
+    }
+}
