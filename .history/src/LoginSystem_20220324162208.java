2d8cbdb3da447750f9f7be42ba1b00416456749d
Ayaan Ahmad <76241848+ayaan278@users.noreply.github.com>
2022-03-24 16:41:41 +0400

    optimised login system matched algo

---
+++ .history/src/LoginSystem_20220324162208.java
@@ -0,0 +1,127 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class LoginSystem {
+    private static Scanner sc;
+
+    //------------------------Read Credentials file------------------------------------
+    private static ArrayList<String> readCredsFromFile(int flag) throws IOException {
+        ArrayList<String> credentials = new ArrayList<>();
+        if(flag==1){
+            List<String> lines = Files.readAllLines(Paths.get("src/credentials/DonorCredentials.csv"));
+            for (int i = 0; i< lines.size();i++) {
+                String[] items = lines.get(i).split(",");
+                String username = items[0];
+                credentials.add(username+" "+items[1]);
+            }
+        }
+        else if(flag==2){
+            List<String> lines = Files.readAllLines(Paths.get("src/credentials/NgoCredentials.csv"));
+            for (int i = 0; i< lines.size();i++) {
+                String[] items = lines.get(i).split(",");
+                String username = items[0];
+                credentials.add(username+" "+items[1]);
+            }
+        } 
+        return credentials;
+    }
+    //------------------------------------------------------------------------------------
+
+    //-----------------------Checks Input username/password with credentials--------------
+    public static void authenticateUser(int flag) throws IOException{
+        sc = new Scanner(System.in);
+    
+        ArrayList<String> credentials = readCredsFromFile(flag);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Enter Name:");
+        String tempname=sc.nextLine();
+        System.out.println("Enter Password:");
+        String temppassword=sc.nextLine();
+        boolean check=false;
+
+        for (int i = 0; i<credentials.size();i++){
+            check =credentials.get(i).equals(tempname+" "+temppassword);
+
+            if(check == true) {
+                System.out.println("---------------------------------------------\n"+
+                                   "   Login was Successful\n"+
+                                   "---------------------------------------------\n");
+                String[] temp = credentials.get(i).split("\\s");
+                tempname=temp[0];
+                temppassword=temp[1];
+                if(flag==1){
+                    donorMenu.showDonormenu(tempname);     
+                }
+                else if(flag==2){
+                    //directs to ngo menu
+                    ngoMenu.welcomeNgo(tempname);
+                }
+                break;               
+            }
+        }
+        if(check==false){
+            System.out.println("---------------------------------------------\n"+
+                               "   Either the Name or the Password was wrong\n"+
+                               "   Try again\n"+
+                               "---------------------------------------------\n");
+            authenticateUser(flag);
+        } 
+    }
+    //------------------------------------------------------------------------------------
+
+    //------------------creates a new usernamein the system------------------------------
+    public static void createNewUsers(int flag) throws IOException {
+    
+    sc = new Scanner(System.in);
+    System.out.println("|-------------------------------------------------------------------|");
+    System.out.println("Please Enter your new Username: ");
+    String username = sc.nextLine();
+    System.out.println("Please Enter your new password");
+    String password = sc.nextLine();
+    System.out.println("Please Enter your password again: ");
+    String temppassword=sc.nextLine();
+    System.out.println("|-------------------------------------------------------------------|");
+
+    do{ 
+        if (temppassword.equals(password)){             // if the temppassword matches the password  will  procced to save credentials to the file
+            break;
+        }
+        else{
+            System.out.println("Passwords don't match. Please Enter your password again");  // will keep looping until both passwords match 
+            temppassword=sc.nextLine();
+        }
+    }while(temppassword!=password);
+
+    saveCredsToFile(flag,username,password);
+    }
+    //------------------------------------------------------------------------------------
+
+    //-------------------Saves the info of new user---------------------------------------
+    private static void saveCredsToFile(int flag, String name, String password) throws IOException {
+        ArrayList<String> credentials = readCredsFromFile(flag);
+        if(flag==1){
+            for(int i=0; i<credentials.size(); i>)
+            try{
+                String filename= "src/credentials/DonorCredentials.csv";
+                FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+                fw.write("\n"+name+","+password);              //appends the string to the file
+                fw.close();
+            }catch(IOException ioe){
+                    System.err.println("IOException: " + ioe.getMessage());
+                }
+        }
+        else if(flag==2){
+            try{
+                String filename= "src/credentials/NgoCredentials.csv";
+                FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+                fw.write("\n"+name+","+password);              //appends the string to the file
+                fw.close();
+            }catch(IOException ioe){
+                    System.err.println("IOException: " + ioe.getMessage());
+                }
+
+        }
+    }
+    //------------------------------------------------------------------------------------  
+}
---
+++ .history/src/matching_20220324155907.java
@@ -0,0 +1,75 @@
+// import java.io.File;
+import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Paths;
+import java.util.*;
+// import java.io.*;
+public class matching{  
+
+    public static void filter() throws IOException{
+        ArrayList<String> donatedaids = Donor.donatedaids();
+        ArrayList<String> requiredaids = Ngo.requiredaids();
+        
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> dName = new ArrayList<String>();
+        ArrayList<String> dAids = new ArrayList<String>();
+        ArrayList<Integer> dQty = new ArrayList<Integer>();
+
+        ArrayList<String> fName = new ArrayList<String>();
+        ArrayList<String> fAids = new ArrayList<String>();
+        ArrayList<Integer> fQty = new ArrayList<Integer>();
+
+        ArrayList<String> nName = new ArrayList<String>();
+        ArrayList<String> nAids = new ArrayList<String>();
+        ArrayList<Integer> nQty = new ArrayList<Integer>();
+        ArrayList<String> nStatus = new ArrayList<String>();
+
+        for (int i =0; i<requiredaids.size();i++){
+             String[] temp = requiredaids.get(i).split("\\s");
+
+             String status = temp[3];
+             if (status.contains("unsatisfied")){
+                 nName.add(temp[0]);
+                 nAids.add(temp[1]);
+                 int Qty = Integer.parseInt(temp[2]);
+                 nQty.add(Qty);
+                 nStatus.add(temp[3]);
+             }
+        }
+        for(int i=0; i< nAids.size();i++){
+            System.out.println(nName.get(i) + " " + nAids.get(i) + " " + nQty.get(i) + " " + nStatus.get(i));
+        }
+
+        System.out.println("Enter the name of the Ngo which requests you want to satisfy: ");
+        String name = sc.nextLine();
+        int flag=0;       
+
+        for (int i =0; i<donatedaids.size();i++){
+            String[] temp = donatedaids.get(i).split("\\s");
+
+            String nameDonor = temp[0];
+            String aids = temp[1];
+            int Qty = Integer.parseInt(temp[2]);
+
+            //System.out.println("aids-"+aids);
+
+            if (aids.contains(name)){
+                dName.add(nameDonor);
+                dAids.add(aids);
+                dQty.add(Qty);
+                flag=1;
+            }
+            else{
+                fName.add(nameDonor);
+                fAids.add(aids);
+                fQty.add(Qty);
+            }
+        }
+        if(flag==0)
+        writeBack.updateDonor(fName, fAids, fQty,0);
+        matchAlgo.runAlgo(name, dName, dAids, dQty, nName, nAids, nQty, nStatus);
+        sc.close();  
+    }  
+
+}
